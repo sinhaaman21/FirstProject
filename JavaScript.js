be89e8688622ab6d9c7c2e8d8c016bb4285f2c3d
Aman <sinha.amansinha@gmail.com>
2018-08-25 14:33:45 +0100

    Jason part1

--- JavaScript.js
+++ JavaScript.js
@@ -1,59 +1,90 @@
-		var plusTranslateX = document.getElementById("slide");
-		var minusTranslateX = document.getElementById("slide");
-		var steps = 320;
-		
-		function moveRight(){
-			var x = getIntFromTransformProperty(plusTranslateX.style['transform'] )-steps;
-			plusTranslateX.style['transform'] = x > -1080 ? transalateX(x) : transalateX(-1080);
-		}
-		
-		function moveLeft(){
-			var x = getIntFromTransformProperty(minusTranslateX.style['transform'] )+steps;
-			minusTranslateX.style['transform'] = x < 0 ? transalateX(x) : transalateX(-10);
-		}
-		
-		function getIntFromTransformProperty(t){
-			return parseInt(t.slice(t.indexOf("(")+1,t.indexOf(")")))||0;
-		}
-		
-		function transalateX(i){
-			return "TranslateX(" + i +"px)"
-		}
-		
-		var arrayImage = ["slider1.png", "slider2.png", "slider1.png", "slider2.png", "slider1.png", "slider2.png", "slider1.png"];
-		var arrayText = ["Text A", "Text B", "Text C", "Text D", "Text E", "Text F", "Text G"];
-		var parent = document.getElementById("slide");
+var places = [{
+		"place": "Banglore",
+		"properties": "1,387 properties",
+		"price": "Average price ₹ 2,140",
+		"image": "banglore.png"
+	},
+	{
+		"place": "Chennai",
+		"properties": "1,387 properties",
+		"price": "Average price ₹ 2,140",
+		"image": "chennai.png"
+	},
+	{
+		"place": "Mumbai",
+		"properties": "1,387 properties",
+		"price": "Average price ₹ 2,140",
+		"image": "mambai.png"
+	},
+	{
+		"place": "New Delhi",
+		"properties": "1,387 properties",
+		"price": "Average price ₹ 2,140",
+		"image": "newdelhi.png"
+	},
+	{
+		"place": "Hyderabad",
+		"properties": "1,387 properties",
+		"price": "Average price ₹ 2,140",
+		"image": "hyderabad.png"
+	}];
 
-		create();
-		
-		function create(){
-		 var html ="";
+var plusTranslateX = document.getElementById("slide");
+var minusTranslateX = document.getElementById("slide");
+var steps = 320;
 
-			for (var i=0; i< arrayImage.length; i++){
-				html +=`<div class="imagelist">
-							<a><img src="`+arrayImage[i]+`">
-								<div>`+arrayText[i]+`</div>
-							</a>
-						 </div>`;
-			}
-			parent.innerHTML = html;
-		}
-			
-			
-			
-			/*	var div = document.createElement("div");
-				div.className="imagelist";
-				parent.appendChild(div);
-				var a = document.createElement("a");
-				div.appendChild(a);
-				var img = document.createElement("img");
-				img.src = arrayImage[i];
-				a.appendChild(img);
-				var div = document.createElement("div");
-				div.innerHTML = arrayText[i];
-				a.appendChild(div);
-			}
-		}*/
+function moveRight(){
+	var x = getIntFromTransformProperty(plusTranslateX.style['transform'] )-steps;
+	plusTranslateX.style['transform'] = x > -1080 ? transalateX(x) : transalateX(-1080);
+}
+
+function moveLeft(){
+	var x = getIntFromTransformProperty(minusTranslateX.style['transform'] )+steps;
+	minusTranslateX.style['transform'] = x < 0 ? transalateX(x) : transalateX(-10);
+}
+
+function getIntFromTransformProperty(t){
+	return parseInt(t.slice(t.indexOf("(")+1,t.indexOf(")")))||0;
+}
+
+function transalateX(i){
+	return "TranslateX(" + i +"px)"
+}
+
+var arrayImage = ["slider1.png", "slider2.png", "slider1.png", "slider2.png", "slider1.png", "slider2.png", "slider1.png"];
+var arrayText = ["Text A", "Text B", "Text C", "Text D", "Text E", "Text F", "Text G"];
+var parent = document.getElementById("slide");
+
+create();
+
+function create(){
+ var html ="";
+
+	for (var i=0; i< arrayImage.length; i++){
+		html +=`<div class="imagelist">
+					<a><img src="`+arrayImage[i]+`">
+						<div>`+arrayText[i]+`</div>
+					</a>
+				 </div>`;
+	}
+	parent.innerHTML = html;
+}
+	
+	
+	
+	/*	var div = document.createElement("div");
+		div.className="imagelist";
+		parent.appendChild(div);
+		var a = document.createElement("a");
+		div.appendChild(a);
+		var img = document.createElement("img");
+		img.src = arrayImage[i];
+		a.appendChild(img);
+		var div = document.createElement("div");
+		div.innerHTML = arrayText[i];
+		a.appendChild(div);
+	}
+}*/
 		
 		
 		
